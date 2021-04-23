7ca70c22a69da6355997fecd8e4fcd811fd1883f
Sunil Singh <singh.sunil7725@gmail.com>
2021-04-23 22:04:22 +0530

    Initial commit

---
+++ app/src/main/java/sunilshoppingapp/com/MainActivity.kt
@@ -0,0 +1,11 @@
+package sunilshoppingapp.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
